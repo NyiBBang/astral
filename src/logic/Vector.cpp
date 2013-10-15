ac8905ad786f65589e6043eaf7fd13419f573e29
Vincent PALANCHER <vincent.palancher@gmail.com>
2013-10-15 07:52:26 +0200

    Correcting bug where normalizing a null vector ended up in a NAN direction

--- src/logic/Vector.cpp
+++ src/logic/Vector.cpp
@@ -8,7 +8,8 @@ LengthVector make_vector(const Position& a, const Position& b)
 
 Direction normalize(const LengthVector& vec)
 {
-    const quantity<length, double> length = root<2>(pow<2>(vec.x) + pow<2>(vec.y));
-    return {vec.x / length, vec.y / length};
+    const quantity<length, double> norm = root<2>(pow<2>(vec.x) + pow<2>(vec.y));
+    if (norm == quantity<length, double>(0)) return {0, 0};
+    return {vec.x / norm, vec.y / norm};
 }
 
--- src/logic_test/VectorTest.cpp
+++ src/logic_test/VectorTest.cpp
@@ -12,3 +12,18 @@ BOOST_AUTO_TEST_CASE(make_vector_creates_a_vector_going_from_a_to_b)
     BOOST_CHECK_EQUAL(actual.y, expected.y);
 }
 
+BOOST_AUTO_TEST_CASE(normalized_vector_has_a_norm_of_1)
+{
+    const LengthVector vec{32 * meters, 25 * meters};
+    const Direction direct = normalize(vec);
+    BOOST_CHECK_CLOSE(1., pow<2>(direct.x) + pow<2>(direct.y), 0.001);
+}
+
+BOOST_AUTO_TEST_CASE(normalized_null_vector_is_null)
+{
+    const LengthVector vec{0, 0};
+    const Direction direct = normalize(vec);
+    BOOST_CHECK_EQUAL(direct.x, 0);
+    BOOST_CHECK_EQUAL(direct.y, 0);
+}
+
