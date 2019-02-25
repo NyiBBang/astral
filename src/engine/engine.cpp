eeb5d01533ab2475a32e476f800817a11644d465
Vincent PALANCHER <vincent.palancher@gmail.com>
2019-02-25 23:39:26 +0100

    Implement naive version of engine::run().

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,17 +1,17 @@
 // Copyright (C) 2018 Vincent PALANCHER (nyibbang)
-// 
+//
 // This file is part of Astral.
-// 
+//
 // Astral is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // Astral is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Astral.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,4 +19,5 @@
 
 namespace astral::eng
 {
-}
+
+} // namespace astral::eng
